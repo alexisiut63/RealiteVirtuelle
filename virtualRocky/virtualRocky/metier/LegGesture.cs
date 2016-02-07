e11d0622d5ae6345bb2c7a904b1b39cbd7e15f7b
Alexis <alexis.caillot@etu.udamail.fr>
2016-02-07 19:59:08 +0100

    essai

---
+++ virtualRocky/virtualRocky/metier/LegGesture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rokinect.metier
+{
+    abstract class LegGesture : Gesture
+    {
+
+        override
+            public abstract void move();
+    }
+}
---
+++ virtualRocky/virtualRocky/metier/SkippingRopeFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rokinect.metier
+{
+    class SkippingRopeFactory : BoxingObjectFactory
+    {
+
+        override
+            public BoxingObject createBO()
+        {
+            return new SkippingRope();
+        }
+    }
+}
