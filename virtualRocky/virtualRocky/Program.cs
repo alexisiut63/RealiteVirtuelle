a65418f38ec03ffb2ac59a7a6021cc6b5076d2f3
Alexis <alexis.caillot@etu.udamail.fr>
2016-02-08 23:19:43 +0100

    essai guard

--- virtualRocky/virtualRocky/Program.cs
+++ virtualRocky/virtualRocky/Program.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Fusion;
 using Rokinect.metier;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using virtualRocky.librayrieGeste;
 
 namespace virtualRocky
 {
@@ -12,22 +14,29 @@ namespace virtualRocky
     {
         static void Main(string[] args)
         {
-            var kinectSensor = KinectSensor.GetDefault();
+            /*  var kinectSensor = KinectSensor.GetDefault();
 
-            // get the coordinate mapper
-            var coordinateMapper = kinectSensor.CoordinateMapper;
+              // get the coordinate mapper
+              var coordinateMapper = kinectSensor.CoordinateMapper;
 
-            // get the depth (display) extents
-            FrameDescription frameDescription = kinectSensor.DepthFrameSource.FrameDescription;
+              // get the depth (display) extents
+              FrameDescription frameDescription = kinectSensor.DepthFrameSource.FrameDescription;
 
-            // get size of joint space
-            var displayWidth = frameDescription.Width;
-            var displayHeight = frameDescription.Height;
+              // get size of joint space
+              var displayWidth = frameDescription.Width;
+              var displayHeight = frameDescription.Height;
 
-            // open the reader for the body frames
-            var bodyFrameReader = kinectSensor.BodyFrameSource.OpenReader();
+              // open the reader for the body frames
+              var bodyFrameReader = kinectSensor.BodyFrameSource.OpenReader();
+              */
+            Vector3 x = new Vector3();
+            x.X = 10;
 
+            Vector3 y = new Vector3();
+            y.X = 30;
 
+            float truc = y.X - x.X;
+            Console.WriteLine(truc);
             Console.ReadKey(true);
             Console.Clear();
         }
--- virtualRocky/virtualRocky/librayrieGeste/Guard.cs
+++ virtualRocky/virtualRocky/librayrieGeste/Guard.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Fusion;
 
 namespace virtualRocky.librayrieGeste
 {
-    class Guard
+    class Guard : Posture
     {
+        public override bool testPosture(Body b)
+        {
+            Joint handR = b.Joints[JointType.HandRight];
+            Joint handL = b.Joints[JointType.HandRight];
+            Joint shoulderR = b.Joints[JointType.HandRight];
+            Joint shoulderL= b.Joints[JointType.HandRight];
+
+
+            Vector3  mainD= new Vector3();
+            mainD.X = handR.Position.X;
+            mainD.Y = handR.Position.Y;
+            mainD.Z = handR.Position.Z;
+
+            Vector3 mainG = new Vector3();
+            mainG.X = handL.Position.X;
+            mainG.Y = handL.Position.Y;
+            mainG.Z = handL.Position.Z;
+
+            Vector3 epauleD= new Vector3();
+            epauleD.X = shoulderR.Position.X;
+            epauleD.Y = shoulderR.Position.Y;
+            epauleD.Z = shoulderR.Position.Z;
+
+            Vector3 epauleG = new Vector3();
+            epauleG.X = shoulderL.Position.X;
+            epauleG.Y = shoulderL.Position.Y;
+            epauleG.Z = shoulderL.Position.Z;
+
+            // soustraction de epaule droite a epaule gauche pour nb positif
+            float mains = mainD.X - mainG.X;
+            if (mains > 15)
+                throw new Exception("mains trop loin");
+
+            float epmainsG = mainG.X - epauleG.X;
+            float epmainsD = epauleD.X - mainD.X;
+
+            
+
+            return true;
+        }
     }
 }
